64e6105c6be80a6f57d872429b7d4c52a2888fe6
zedd <wangjidw@gmail.com>
2021-11-09 01:12:42 +0700

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = jsonServer.router('./data.json');
 const middlewares = jsonServer.defaults({
   static: './build'
 });
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(jsonServer.rewriter({
   '/api/*': '/$1',
@@ -13,3 +13,4 @@ server.use(router);
 server.listen(PORT, () => {
   console.log('Server is running');
 });
+
