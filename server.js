6e2b568be8398f5cbf080005e15acbdf6ecde9ea
zedd <wangjidw@gmail.com>
2021-11-09 01:26:16 +0700

    update

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./data.json');
-const middlewares = jsonServer.defaults({
-  static: './build'
-});
+const router = jsonServer.router('data.json');
+const middlewares = jsonServer.defaults()
+
 const PORT = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(jsonServer.rewriter({
